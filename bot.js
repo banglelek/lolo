ce61e7d3464b7b0329921171f5aaa3b34e5f8dd1
banglelek <46139812+banglelek@users.noreply.github.com>
2019-01-13 10:50:15 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("526927433077882880")
+setInterval(function() {
+channel.send(` etyrtyrtyrtyrtyrtytryrtylek melekreytryrtytrytrgte5rgterrtytryrtgteytryrtyrtytrrtgr bang 2vv ';lpk45345L  `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
